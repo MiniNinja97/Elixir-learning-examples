65bd12ac6bc5a5ad6812273ffdeeb4dd65a3f985
Emma <emma.malinsdotter@outlook.com>
2025-08-06 14:49:57 +0200

    la till lista

--- lib/tuples.ex
+++ lib/tuples.ex
@@ -9,11 +9,33 @@ defmodule MyTuples do
 
     IO.puts("Average price from #{elem(memberships,1)} #{elem(memberships, 1)} #{elem(memberships, 2)} is #{avg}")
 
-    user1 = {"John", :gold}
 
-    {name, membership} = user1
 
-    IO.puts("#{name} has a #{membership} membership")
+
+
+    # user1 = {"John", :gold}
+    # user2 = {"Kim", :silver}
+    # user3 = {"Lisa", :bronze}
+
+    # {name, membership} = user1
+    # IO.puts("#{name} has a #{membership} membership")
+
+    # {name, membership} = user2
+    # IO.puts("#{name} has a #{membership} membership")
+
+    # {name, membership} = user3
+    # IO.puts("#{name} has a #{membership} membership")
+
+  users =[
+    {"John", :gold},
+    {"Kim", :silver},
+    {"Lisa", :bronze}
+  ]
+
+  Enum.each(users, fn {name, membership} -> IO.puts("#{name} has a #{membership} membership") end)
+
+
+
   end
 
 end
